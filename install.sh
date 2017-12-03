437ef17707210318f5273bf34ac38b92dc161fad
langyxxl <1150283637@qq.com>
2017-12-03 18:52:08 +0800

    add erlang support

--- install.sh
+++ install.sh
@@ -91,9 +91,9 @@ cd $CURRENT_DIR/bundle/YouCompleteMe/
 git submodule update --init --recursive
 if [ `which clang` ]   # check system clang
 then
-    python install.py --clang-completer --system-libclang   # use system clang
+    python install.py --all --system-libclang   # use system clang
 else
-    python install.py --clang-completer
+    python install.py --all
 fi
 
 echo "Install Done!"
